2ab51cf7b1fd5cb3174d9b42e6f8ef5605491cb8
Hossein HassanNejad <hosseinspell@gmail.com>
2021-02-15 17:07:34 +0330

    navigation bar

--- lib/widget/navigation_bar.dart
+++ lib/widget/navigation_bar.dart
@@ -3,9 +3,11 @@ import 'package:flutter/material.dart';
 class NavigationBar extends StatefulWidget {
   Color unselectedLabelColor;
   bool showUnselectedLabels;
+  Color bgColor;
   NavigationBar(
       {this.unselectedLabelColor = const Color(0xffA0A3B1),
-      this.showUnselectedLabels = true});
+      this.showUnselectedLabels = true,
+      this.bgColor = Colors.white});
   @override
   _NavigationBarState createState() => _NavigationBarState();
 }
@@ -21,14 +23,14 @@ class _NavigationBarState extends State<NavigationBar> {
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      showUnselectedLabels: true,
+      showUnselectedLabels: widget.showUnselectedLabels,
       unselectedItemColor: widget.unselectedLabelColor,
       items: <BottomNavigationBarItem>[
-        btmNvBarItem(icon: Icons.home_outlined, title: 'Home'),
-        btmNvBarItem(icon: Icons.nights_stay_outlined, title: 'Sleep'),
-        btmNvBarItem(icon: Icons.donut_small_outlined, title: 'Meditate'),
-        btmNvBarItem(icon: Icons.music_note_outlined, title: 'Music'),
-        btmNvBarItem(icon: Icons.person_outline, title: 'Profile'),
+        btmNvBarItem(icon: "images/home.png", title: 'Home'),
+        btmNvBarItem(icon: "images/sleep.png", title: 'Sleep'),
+        btmNvBarItem(icon: "images/meditate.png", title: 'Meditate'),
+        btmNvBarItem(icon: "images/music.png", title: 'Music'),
+        btmNvBarItem(icon: "images/profile.png", title: 'Profile'),
       ],
       currentIndex: _selectedIndex,
       selectedItemColor: Color(0xff8E97FD),
@@ -36,32 +38,42 @@ class _NavigationBarState extends State<NavigationBar> {
     );
   }
 
-  BottomNavigationBarItem btmNvBarItem({
-    icon = Icons.rounded_corner,
-    Color iconColor = const Color(0xffA0A3B1),
-    double iconPadding = 8.0,
-    String title = 'title',
-  }) {
+  BottomNavigationBarItem btmNvBarItem(
+      {icon = "images/home.png",
+      Color iconColor = const Color(0xffA0A3B1),
+      double iconPadding = 8.0,
+      String title = 'title',
+      Color selectedBoxbg = const Color(0xff8E97FD),
+      Color selectedIconColor = Colors.white}) {
     return BottomNavigationBarItem(
       icon: Padding(
-        padding: EdgeInsets.all(iconPadding),
-        child: Icon(
-          icon,
-          color: iconColor,
-        ),
-      ),
+          padding: EdgeInsets.all(iconPadding),
+          child: ImageIcon(
+            AssetImage(icon),
+            color: iconColor,
+          )),
       label: title,
       activeIcon: Padding(
         padding: EdgeInsets.all(8.0),
         child: ClipRRect(
             borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(8.0),
-              topRight: Radius.circular(8.0),
-              bottomLeft: Radius.circular(8.0),
-              bottomRight: Radius.circular(8.0),
+              topLeft: Radius.circular(15.0),
+              topRight: Radius.circular(15.0),
+              bottomLeft: Radius.circular(15.0),
+              bottomRight: Radius.circular(15.0),
             ),
-            child: Icon(Icons.home)),
+            child: Container(
+              color: selectedBoxbg,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ImageIcon(
+                  AssetImage(icon),
+                  color: selectedIconColor,
+                ),
+              ),
+            )),
       ),
+      backgroundColor: widget.bgColor,
     );
   }
 }
