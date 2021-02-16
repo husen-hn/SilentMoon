358af8e490b6973889a5d0143f48b8c2656e288e
Hossein HassanNejad <hosseinspell@gmail.com>
2021-02-16 11:49:15 +0330

    README ☷

--- README.md
+++ README.md
@@ -1,16 +1,36 @@
-# feedTest
+<img src="preview/header.png" alt="SilentMoon header"  height="450">
 
-A new Flutter project.
+## Silent Moon ⌒
+meditation app, Explore the app, Find some peace of mind to prepare for meditation.
+using [Flutter](https://flutter.dev) and [MUSOPEN](https://musopen.org/) music services.
 
-## Getting Started
+## Features ⌑
+* Meditation Moods (sleep, relaxation, ...)
 
-This project is a starting point for a Flutter application.
+## Previews ⛶
 
-A few resources to get you started if this is your first Flutter project:
+<img src="preview/sc1.png" alt="screenshots"  height="350" width="160"> <img src="preview/sc2.png" alt="screenshots"  height="350" width="160"> <img src="preview/sc3.png" alt="screenshots"  height="350" width="160"> <img src="preview/sc4.png" alt="screenshots"  height="350" width="160"> <img src="preview/sc5.png" alt="screenshots"  height="350" width="160">
 
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+## Contribute ༜
+Want to contribute? I would really appreciate a hand with the development to add more features in this app.
+Feel free to Fork, edit, then pull!
 
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+## Feature roadmap ⌲
+* [x] UI
+* [ ] MVVM
+* [x] BloC
+* [ ] Accessibility
+* [ ] Provider
+* [ ] Dark Mode
+* [ ] Animation
+* [ ] Google Sign in
+* [ ] Notification
+
+<!-- ## Download ⇣
+[<img src="preview/get-cafebazaar.webp" alt="Download from Cafebazaar" height="50">](https://cafebazaar.ir/app/com.husen.android.bitgram) [<img src="preview/direct-apk-download.webp" alt="Direct apk download"  height="51.5">](https://github.com/husen-hn/Bitgram/releases/latest) -->
+
+## License ©
+```
+Copyright © 2021 Hossein HassanNejad 
+Licensed under the GNU Version 3 license.
+```
--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -8,6 +8,102 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: ListView(
+        children: [
+          // Title
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Silent',
+                  style: TextStyle(
+                      fontSize: 16.0,
+                      letterSpacing: 2.0,
+                      color: Color(0xff3F414E)),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image(
+                    image: AssetImage("images/logo.png"),
+                  ),
+                ),
+                Text(
+                  'Moon',
+                  style: TextStyle(
+                      fontSize: 16.0,
+                      letterSpacing: 2.0,
+                      color: Color(0xff3F414E)),
+                ),
+              ],
+            ),
+          ),
+          // Welcome
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Text('Good Morning,',
+                style: TextStyle(
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xff3F414E))),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+                top: 8.0, right: 8.0, left: 8.0, bottom: 12.0),
+            child: Text('We Wish you have a good day',
+                style: TextStyle(fontSize: 20.0, color: Color(0xffA1A4B2))),
+          ),
+          // Basics & Relaxation
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    width: 177,
+                    height: 210,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      color: Color(0xff8E97FD),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        // Container top Icon
+                        Align(
+                          alignment: Alignment.topRight,
+                          child: Image(
+                            image: AssetImage("images/apple.png"),
+                          ),
+                        ),
+                        Text('Basics'),
+                        Text('COURSE'),
+                        Row(
+                          children: [
+                            Text('3-10 MIN'),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 15.0,
+                ),
+                Expanded(
+                  child: Container(
+                    width: 177,
+                    height: 210,
+                    color: Color(0xffFFC97E),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
