6a3d4487be91b77df73e81f753c47729b594fe54
Hossein HassanNejad <hosseinspell@gmail.com>
2021-03-24 10:18:29 +0430

    recent changes

--- lib/screen/nav_bar_screens/music.dart
+++ lib/screen/nav_bar_screens/music.dart
@@ -36,226 +36,231 @@ class _MusicState extends State<Music> with SingleTickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: CustomScrollView(
-        slivers: [
-          //top images
-          Theme(
-            data: Theme.of(context).copyWith(
-                accentColor: Color(0xff8E97FD), primaryColor: Colors.white),
-            child: SliverAppBar(
-              expandedHeight: 350,
-              automaticallyImplyLeading: false,
-              flexibleSpace: FlexibleSpaceBar(
-                background: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    //top image
-                    Container(
-                      height: 250.0,
-                      decoration: const BoxDecoration(
-                        borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(10),
-                            bottomRight: Radius.circular(10)),
-                        image: DecorationImage(
-                            image: AssetImage("images/man_meditation.png"),
-                            fit: BoxFit.fill),
-                      ),
-                    ),
-                    //title & description
-                    Padding(
-                      padding: const EdgeInsets.only(top: 5.0, left: 10.0),
-                      child: RichText(
-                        text: TextSpan(
-                          text: 'Music\n',
-                          style: TextStyle(
-                              color: Color(0xff3F414E),
-                              fontSize: 34,
-                              fontWeight: FontWeight.bold),
-                          children: <TextSpan>[
-                            TextSpan(
-                                text: 'music\n',
-                                style: TextStyle(
-                                  height: 3.0,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 14.0,
-                                  color: Color(0xffA1A4B2),
-                                )),
-                            TextSpan(
-                                text: 'Description',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.normal,
-                                    fontSize: 13.0,
-                                    color: Color(0xffA1A4B2))),
-                          ],
+    return Theme(
+      data: Theme.of(context).copyWith(accentColor: Color(0xff8E97FD)),
+      child: DefaultTabController(
+        length: 2,
+        child: CustomScrollView(
+          slivers: [
+            //top images
+            Theme(
+              data: Theme.of(context).copyWith(
+                  accentColor: Color(0xff8E97FD), primaryColor: Colors.white),
+              child: SliverAppBar(
+                expandedHeight: 350,
+                automaticallyImplyLeading: false,
+                flexibleSpace: FlexibleSpaceBar(
+                  background: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      //top image
+                      Container(
+                        height: 250.0,
+                        decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(10),
+                              bottomRight: Radius.circular(10)),
+                          image: DecorationImage(
+                              image: AssetImage("images/man_meditation.png"),
+                              fit: BoxFit.fill),
                         ),
                       ),
-                    )
-                  ],
+                      //title & description
+                      Padding(
+                        padding: const EdgeInsets.only(top: 5.0, left: 10.0),
+                        child: RichText(
+                          text: TextSpan(
+                            text: 'Music\n',
+                            style: TextStyle(
+                                color: Color(0xff3F414E),
+                                fontSize: 34,
+                                fontWeight: FontWeight.bold),
+                            children: <TextSpan>[
+                              TextSpan(
+                                  text: 'music\n',
+                                  style: TextStyle(
+                                    height: 3.0,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 14.0,
+                                    color: Color(0xffA1A4B2),
+                                  )),
+                              TextSpan(
+                                  text: 'Description',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.normal,
+                                      fontSize: 13.0,
+                                      color: Color(0xffA1A4B2))),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-          //TabBar
-          Theme(
-            data: Theme.of(context).copyWith(
-                accentColor: Color(0xff8E97FD), primaryColor: Colors.white),
-            child: SliverAppBar(
-              elevation: 0,
-              snap: false,
-              pinned: true,
-              floating: true,
-              bottom: TabBar(
-                controller: _tabController,
-                isScrollable: false,
-                labelColor: Color(0xff8E97FD),
-                unselectedLabelColor: Color(0xffA1A4B2),
-                indicator: UnderlineTabIndicator(
-                    borderSide:
-                        BorderSide(width: 2.0, color: Color(0xff8E97FD)),
-                    insets: EdgeInsets.symmetric(horizontal: 70.0)),
-                tabs: [
-                  Tab(
-                    child: Text(
-                      'VOICES',
-                      style: TextStyle(letterSpacing: 1.0),
+            //TabBar
+            Theme(
+              data: Theme.of(context).copyWith(
+                  accentColor: Color(0xff8E97FD), primaryColor: Colors.white),
+              child: SliverAppBar(
+                elevation: 0,
+                snap: false,
+                pinned: true,
+                floating: true,
+                bottom: TabBar(
+                  controller: _tabController,
+                  isScrollable: false,
+                  labelColor: Color(0xff8E97FD),
+                  unselectedLabelColor: Color(0xffA1A4B2),
+                  indicator: UnderlineTabIndicator(
+                      borderSide:
+                          BorderSide(width: 2.0, color: Color(0xff8E97FD)),
+                      insets: EdgeInsets.symmetric(horizontal: 70.0)),
+                  tabs: [
+                    Tab(
+                      child: Text(
+                        'VOICES',
+                        style: TextStyle(letterSpacing: 1.0),
+                      ),
                     ),
-                  ),
-                  Tab(
-                    child: Text(
-                      "FAVORITE",
-                      style: TextStyle(letterSpacing: 1.0),
+                    Tab(
+                      child: Text(
+                        "FAVORITE",
+                        style: TextStyle(letterSpacing: 1.0),
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
+                expandedHeight: 30,
               ),
-              expandedHeight: 30,
             ),
-          ),
-          //Voice List
-          SliverFixedExtentList(
-            itemExtent: 70.0,
-            delegate:
-                SliverChildBuilderDelegate((BuildContext context, int index) {
-              return TabBarView(
-                controller: _tabController,
-                children: [
-                  // Voice Tab
-                  Container(
-                    padding: EdgeInsets.only(bottom: 10, left: 10, top: 10),
-                    color: Colors.white,
-                    child: InkWell(
-                      child: Column(
-                        children: [
-                          Row(
-                            children: [
-                              // play btn
-                              Container(
-                                width: 40.0,
-                                height: 40.0,
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(100),
-                                    border: Border.all(
-                                        width: 1, color: Color(0xffA1A4B2))),
-                                child: IconButton(
-                                  autofocus: true,
-                                  icon: Image(
-                                    image: AssetImage("images/play.png"),
-                                    color: Color(0xffA1A4B2),
-                                    width: 12.0,
-                                  ),
-                                  onPressed: () {},
-                                ),
-                              ),
-                              // voice title & time
-                              Padding(
-                                padding: const EdgeInsets.only(left: 10.0),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      musics[index]["name"],
-                                      style: TextStyle(
-                                          color: Color(0xff3F414E),
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 16.0),
+            //Voice List
+            SliverFixedExtentList(
+              itemExtent: 70.0,
+              delegate:
+                  SliverChildBuilderDelegate((BuildContext context, int index) {
+                return TabBarView(
+                  controller: _tabController,
+                  children: [
+                    // Voice Tab
+                    Container(
+                      padding: EdgeInsets.only(bottom: 10, left: 10, top: 10),
+                      color: Colors.white,
+                      child: InkWell(
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                // play btn
+                                Container(
+                                  width: 40.0,
+                                  height: 40.0,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(100),
+                                      border: Border.all(
+                                          width: 1, color: Color(0xffA1A4B2))),
+                                  child: IconButton(
+                                    autofocus: true,
+                                    icon: Image(
+                                      image: AssetImage("images/play.png"),
+                                      color: Color(0xffA1A4B2),
+                                      width: 12.0,
                                     ),
-                                    Text(
-                                      musics[index]["time"],
-                                      style: TextStyle(
-                                          color: Color(0xffA1A4B2),
-                                          fontSize: 11.0),
-                                    )
-                                  ],
+                                    onPressed: () {},
+                                  ),
                                 ),
-                              ),
-                            ],
-                          ),
-                        ],
+                                // voice title & time
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 10.0),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        musics[index]["name"],
+                                        style: TextStyle(
+                                            color: Color(0xff3F414E),
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 16.0),
+                                      ),
+                                      Text(
+                                        musics[index]["time"],
+                                        style: TextStyle(
+                                            color: Color(0xffA1A4B2),
+                                            fontSize: 11.0),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                  // Favorite Tab
-                  Container(
-                    padding: EdgeInsets.only(bottom: 10, left: 10, top: 10),
-                    color: Colors.white,
-                    child: InkWell(
-                      child: Column(
-                        children: [
-                          Row(
-                            children: [
-                              // play btn
-                              Container(
-                                width: 40.0,
-                                height: 40.0,
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(100),
-                                    border: Border.all(
-                                        width: 1, color: Color(0xffA1A4B2))),
-                                child: IconButton(
-                                  autofocus: true,
-                                  icon: Image(
-                                    image: AssetImage("images/play.png"),
-                                    color: Color(0xffA1A4B2),
-                                    width: 12.0,
-                                  ),
-                                  onPressed: () {},
-                                ),
-                              ),
-                              // voice title & time
-                              Padding(
-                                padding: const EdgeInsets.only(left: 10.0),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      favMusics[index]["name"],
-                                      style: TextStyle(
-                                          color: Color(0xff3F414E),
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 16.0),
+                    // Favorite Tab
+                    Container(
+                      padding: EdgeInsets.only(bottom: 10, left: 10, top: 10),
+                      color: Colors.white,
+                      child: InkWell(
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                // play btn
+                                Container(
+                                  width: 40.0,
+                                  height: 40.0,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(100),
+                                      border: Border.all(
+                                          width: 1, color: Color(0xffA1A4B2))),
+                                  child: IconButton(
+                                    autofocus: true,
+                                    icon: Image(
+                                      image: AssetImage("images/play.png"),
+                                      color: Color(0xffA1A4B2),
+                                      width: 12.0,
                                     ),
-                                    Text(
-                                      favMusics[index]["time"],
-                                      style: TextStyle(
-                                          color: Color(0xffA1A4B2),
-                                          fontSize: 11.0),
-                                    )
-                                  ],
+                                    onPressed: () {},
+                                  ),
                                 ),
-                              ),
-                            ],
-                          ),
-                        ],
+                                // voice title & time
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 10.0),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        favMusics[index]["name"],
+                                        style: TextStyle(
+                                            color: Color(0xff3F414E),
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 16.0),
+                                      ),
+                                      Text(
+                                        favMusics[index]["time"],
+                                        style: TextStyle(
+                                            color: Color(0xffA1A4B2),
+                                            fontSize: 11.0),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              );
-            }, childCount: favMusics.length),
-          ),
-        ],
+                  ],
+                );
+              }, childCount: favMusics.length),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/screen/nav_bar_screens/profile/recent_changes.dart
+++ lib/screen/nav_bar_screens/profile/recent_changes.dart
@@ -16,76 +16,80 @@ class _RecentChangesState extends State<RecentChanges> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Recent Changes',
-          style: TextStyle(color: const Color(0xff3F414E)),
+    return Theme(
+      data: Theme.of(context).copyWith(accentColor: Color(0xff8E97FD)),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Recent Changes',
+            style: TextStyle(color: const Color(0xff3F414E)),
+          ),
+          iconTheme: IconThemeData(color: const Color(0xff3F414E)),
+          centerTitle: true,
+          elevation: 0,
+          backgroundColor: Colors.grey[50],
         ),
-        iconTheme: IconThemeData(color: const Color(0xff3F414E)),
-        centerTitle: true,
-        elevation: 0,
-        backgroundColor: Colors.grey[50],
-      ),
-      body: ListView.builder(
-          itemCount: changesList.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Container(
-              margin: EdgeInsets.only(left: 20.0, right: 20.0),
-              child: Row(
-                children: [
-                  Column(
-                    children: [
-                      // circule
-                      Container(
-                        height: 10.0,
-                        width: 10.0,
-                        padding: EdgeInsets.all(8.0),
-                        decoration: BoxDecoration(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(100)),
-                            color: const Color(0xff3F414E)),
-                      ),
-                      // vertical line
-                      Container(
-                        height: changesList[index].changes.length * 30.0,
-                        width: 2.0,
-                        color: const Color(0xff3F414E),
-                      ),
-                    ],
-                  ),
-                  // version & changes
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.only(left: 10.0),
-                        child: Text(
-                          'version ${changesList[index].version}',
-                          style: TextStyle(fontWeight: FontWeight.bold),
+        body: ListView.builder(
+            itemCount: changesList.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                margin: EdgeInsets.only(left: 20.0, right: 20.0),
+                child: Row(
+                  children: [
+                    Column(
+                      children: [
+                        // circule
+                        Container(
+                          height: 10.0,
+                          width: 10.0,
+                          padding: EdgeInsets.all(8.0),
+                          decoration: BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(100)),
+                              color: const Color(0xff3F414E)),
                         ),
-                      ),
-                      Container(
-                        height: changesList[index].changes.length * 30.0,
-                        width: MediaQuery.of(context).size.width * .8,
-                        padding: EdgeInsets.only(left: 20.0, top: 10.0),
-                        child: ListView.builder(
-                            physics: NeverScrollableScrollPhysics(),
-                            itemCount: changesList[index].changes.length,
-                            itemBuilder: (BuildContext context, int subIndex) {
-                              return Padding(
-                                padding: const EdgeInsets.only(bottom: 5.0),
-                                child:
-                                    Text(changesList[index].changes[subIndex]),
-                              );
-                            }),
-                      )
-                    ],
-                  )
-                ],
-              ),
-            );
-          }),
+                        // vertical line
+                        Container(
+                          height: changesList[index].changes.length * 30.0,
+                          width: 2.0,
+                          color: const Color(0xff3F414E),
+                        ),
+                      ],
+                    ),
+                    // version & changes
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(left: 10.0),
+                          child: Text(
+                            'version ${changesList[index].version}',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Container(
+                          height: changesList[index].changes.length * 30.0,
+                          width: MediaQuery.of(context).size.width * .8,
+                          padding: EdgeInsets.only(left: 20.0, top: 10.0),
+                          child: ListView.builder(
+                              physics: NeverScrollableScrollPhysics(),
+                              itemCount: changesList[index].changes.length,
+                              itemBuilder:
+                                  (BuildContext context, int subIndex) {
+                                return Padding(
+                                  padding: const EdgeInsets.only(bottom: 5.0),
+                                  child: Text(
+                                      changesList[index].changes[subIndex]),
+                                );
+                              }),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }),
+      ),
     );
   }
 }
