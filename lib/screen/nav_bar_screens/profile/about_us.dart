3adcb499d50dd1df51e4654e0ddfcf64cbb2cc61
Hossein HassanNejad <hosseinspell@gmail.com>
2021-03-25 12:14:39 +0430

    about us

--- lib/screen/nav_bar_screens/profile/about_us.dart
+++ lib/screen/nav_bar_screens/profile/about_us.dart
@@ -1,4 +1,6 @@
+import 'package:SilentMoon/widget/profile_btn.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AboutUs extends StatefulWidget {
   @override
@@ -19,7 +21,55 @@ class _AboutUsState extends State<AboutUs> {
         elevation: 0,
         backgroundColor: Colors.grey[50],
       ),
-      body: Container(),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Text(
+              'meditation app, Explore the app, Find some peace of mind to prepare for meditation. using Flutter and MUSOPEN music services.',
+              style: TextStyle(fontSize: 14.0),
+            ),
+          ),
+          ProfileBtn(
+              btnTitle: 'MUSOPEN',
+              btnColor: const Color(0xffB6B8BF),
+              btnTxtColor: const Color(0xff3F414E),
+              icon: Icons.keyboard_arrow_right,
+              onTap: this._onBtnTap),
+          ProfileBtn(
+              btnTitle: 'GitHub',
+              btnColor: const Color(0xffB6B8BF),
+              btnTxtColor: const Color(0xff3F414E),
+              icon: Icons.keyboard_arrow_right,
+              onTap: this._onBtnTap),
+          ProfileBtn(
+              btnTitle: 'Twitter',
+              btnColor: const Color(0xffB6B8BF),
+              btnTxtColor: const Color(0xff3F414E),
+              icon: Icons.keyboard_arrow_right,
+              onTap: this._onBtnTap),
+          ProfileBtn(
+              btnTitle: 'LICENSE',
+              btnColor: const Color(0xffB6B8BF),
+              btnTxtColor: const Color(0xff3F414E),
+              icon: Icons.keyboard_arrow_right,
+              onTap: this._onBtnTap)
+        ],
+      ),
     );
   }
+
+  _onBtnTap(String btnTitle) {
+    if (btnTitle == 'MUSOPEN') {
+      _launchURL('https://musopen.org/');
+    } else if (btnTitle == 'GitHub') {
+      _launchURL('https://github.com/husen-hn/SilentMoon');
+    } else if (btnTitle == 'Twitter') {
+      _launchURL('https://twitter.com/husen_hn');
+    } else if (btnTitle == 'LICENSE') {
+      _launchURL('https://github.com/husen-hn/SilentMoon/blob/master/LICENSE');
+    }
+  }
+
+  void _launchURL(String url) async => await launch(url);
 }
